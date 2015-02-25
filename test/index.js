26dd5b4360d70de6c77da43f12b3b50ffe96ce27
Ionică Bizău <bizauionica@yahoo.com>
2015-02-25 09:00:01 +0200

    Comments

--- test/index.js
+++ test/index.js
@@ -1,12 +1,17 @@
+// Dependencies
 var Http = require("http")
   , PageChanged = require("../lib")
   ;
 
+// Constants
 const PORT = 8000;
+
+// Create a http server
 Http.createServer(function (req, res) {
     res.end(Math.floor(new Date().getTime() / 1000).toString());
 }).listen(PORT);
 
+// Detect changes
 var pc = new PageChanged({
     page: "http://localhost:" + PORT
   , interval: 1000
