3d930003dc1f426d87fcd5fd3a29fb990fc6545a
Ionică Bizău <bizauionica@gmail.com>
2016-05-25 15:45:07 +0300

    Fix a bug that appears in strict mode.
    `this` is undefined when not calling the function using `new`, so use `typpy` to check if the context is a current instance, otherwise, create one. :boom:

--- example/index.js
+++ example/index.js
@@ -1,20 +1,21 @@
-// Dependencies
-var Http = require("http")
-  , PageChanged = require("../lib")
-  ;
+"use strict";
+
+const http = require("http")
+    , PageChanged = require("../lib")
+    ;
 
 // Constants
 const PORT = 8000;
 
 // Create a http server
-Http.createServer(function (req, res) {
+http.createServer((req, res) => {
     res.end(Math.floor(new Date().getTime() / 1000).toString());
 }).listen(PORT);
 
 // Detect changes
-var pc = new PageChanged({
+let pc = new PageChanged({
     page: "http://localhost:" + PORT
   , interval: 1000
-}, function (err, html) {
+}, (err, html) => {
     console.log(html);
 });
